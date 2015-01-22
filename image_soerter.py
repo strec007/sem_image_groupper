fd07a85f83ec15fd50aa8c28db83223b49f7409d
Petr Cizmar <petr@cizmar.org>
2015-01-22 09:19:06 +0100

    initial commit
    Signed-off-by: Petr Cizmar <petr@cizmar.org>

---
+++ image_soerter.py
@@ -0,0 +1,17 @@
+import tifffile as tiff
+import os
+import sys
+
+def process_image(filename):
+  a = tiff.imread(filename)
+
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    dirname = sys.argv[1]
+  else:
+    dirname = "."
+  for fn in os.listdir(dirname):
+    print fn
+
+
